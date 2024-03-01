d7a675c20abfc8e8dcc31acccf9162e521cc2112
germanp007 <pintogerman281@gmail.com>
2024-03-01 10:44:09 -0300

    arreglando ruta de imagenes

--- teltech/src/components/navbar/Mobile.jsx
+++ teltech/src/components/navbar/Mobile.jsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
 import LogoComponent from "./LogoNav";
-
+import burguerMenu from "../../../public/logo/menu_2976215.png";
 const Mobile = ({ handleClick }) => {
   return (
     <>
       <nav className="h-20 w-full flex justify-between items-center navResponsive:hidden">
         <LogoComponent width={120} height={100} />
         <button onClick={handleClick}>
-          <img
-            src="../public/logo/menu_2976215.png"
-            alt="menu"
-            className="w-8 h-8 mr-8"
-          />
+          <img src={burguerMenu} alt="menu" className="w-8 h-8 mr-8" />
         </button>
       </nav>
     </>
